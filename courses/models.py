e0ae6ae31a47b0b2e63c7ffbaeefd5acd5c3811c
amosISA <amosisa700@gmail.com>
2018-01-26 11:28:47 +0100

    E-learning web with Django

---
+++ courses/models.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Subject(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ('title',)
+
+    def __unicode__(self):
+        return self.title
+
+class Course(models.Model):
+    owner = models.ForeignKey(User, related_name='courses_created')
+    subject = models.ForeignKey(Subject, related_name='courses')
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    overview = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __unicode__(self):
+        return self.title
+
+class Module(models.Model):
+    course = models.ForeignKey(Course, related_name='modules')
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+    def __unicode__(self):
+        return self.title
+
